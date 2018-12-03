b4d9a1b4a8ecd752a450c4a06583e74e8a0a7e7d
Kasper Hareskov Tygesen <kaht@ok.dk>
2018-12-03 16:30:36 +0100

    path fix

--- linux-vm-docker-dotnetcore-nodejs/scripts/install.sh
+++ linux-vm-docker-dotnetcore-nodejs/scripts/install.sh
@@ -1,11 +1,11 @@
 sudo mkdir install-scripts
 cd install-scripts
 
-sudo curl -O https://github.com/ok-amba/OK.Azure.DevTestLabs/master/linux-vm-docker-dotnetcore-nodejs/scripts/linux/install-docker.sh
-sudo curl -O https://github.com/ok-amba/OK.Azure.DevTestLabs/master/linux-vm-docker-dotnetcore-nodejs/scripts/linux/install-nodejs.sh
-sudo curl -O https://github.com/ok-amba/OK.Azure.DevTestLabs/master/linux-vm-docker-dotnetcore-nodejs/scripts/linux/install-dotnetcore.sh
-sudo curl -O https://github.com/ok-amba/OK.Azure.DevTestLabs/master/linux-vm-docker-dotnetcore-nodejs/scripts/linux/install-git.sh
-sudo curl -O https://github.com/ok-amba/OK.Azure.DevTestLabs/master/linux-vm-docker-dotnetcore-nodejs/scripts/linux/install-azure-cli.sh
+sudo curl -O https://raw.githubusercontent.com/ok-amba/OK.Azure.DevTestLabs/master/linux-vm-docker-dotnetcore-nodejs/scripts/install-docker.sh
+sudo curl -O https://raw.githubusercontent.com/ok-amba/OK.Azure.DevTestLabs/master/linux-vm-docker-dotnetcore-nodejs/scripts/install-nodejs.sh
+sudo curl -O https://raw.githubusercontent.com/ok-amba/OK.Azure.DevTestLabs/master/linux-vm-docker-dotnetcore-nodejs/scripts/install-dotnetcore.sh
+sudo curl -O https://raw.githubusercontent.com/ok-amba/OK.Azure.DevTestLabs/master/linux-vm-docker-dotnetcore-nodejs/scripts/install-git.sh
+sudo curl -O https://raw.githubusercontent.com/ok-amba/OK.Azure.DevTestLabs/master/linux-vm-docker-dotnetcore-nodejs/scripts/install-azure-cli.sh
 
 sudo chmod +x install-docker.sh
 sudo chmod +x install-nodejs.sh
