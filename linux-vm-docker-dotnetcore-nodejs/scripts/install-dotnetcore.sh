15de68d7d77fd1da0943fccc39c88eee9f548089
Kasper Hareskov Tygesen <kaht@ok.dk>
2019-02-22 10:23:39 +0100

    dotnet core 2.2

--- linux-vm-docker-dotnetcore-nodejs/scripts/install-dotnetcore.sh
+++ linux-vm-docker-dotnetcore-nodejs/scripts/install-dotnetcore.sh
@@ -4,4 +4,4 @@ sudo dpkg -i packages-microsoft-prod.deb
 
 sudo apt-get install -y apt-transport-https
 sudo apt-get update
-sudo apt-get install -y dotnet-sdk-2.1
+sudo apt-get install -y dotnet-sdk-2.2
