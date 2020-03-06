79f9402333c19b1aa7c2109e7f67e9e434b41931
dlorent <33084168+dlorent@users.noreply.github.com>
2020-03-06 13:44:42 +0100

    moved apt fix out

--- linux-vm-docker-dotnetcore-nodejs/scripts/install-chrome.sh
+++ linux-vm-docker-dotnetcore-nodejs/scripts/install-chrome.sh
@@ -6,7 +6,7 @@ export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 
 sudo apt-get update
-sudo apt --fix-broken install -y
+
 sudo apt-get install -y libappindicator1 fonts-liberation libasound2 libgconf-2-4 libnspr4 libxss1 libnss3 xdg-utils
 
 wget -q  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P $WORKING_DIR/_tmp
