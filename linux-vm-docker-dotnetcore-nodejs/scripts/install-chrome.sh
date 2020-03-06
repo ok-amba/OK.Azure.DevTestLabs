7aa7a03c63ac1fbc19e957720a6fcddf78bc2650
dlorent <33084168+dlorent@users.noreply.github.com>
2020-03-06 13:43:11 +0100

    changed order of commands

--- linux-vm-docker-dotnetcore-nodejs/scripts/install-chrome.sh
+++ linux-vm-docker-dotnetcore-nodejs/scripts/install-chrome.sh
@@ -6,9 +6,9 @@ export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 
 sudo apt-get update
+sudo apt --fix-broken install -y
 sudo apt-get install -y libappindicator1 fonts-liberation libasound2 libgconf-2-4 libnspr4 libxss1 libnss3 xdg-utils
 
 wget -q  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P $WORKING_DIR/_tmp
 
-sudo apt --fix-broken install -y
 sudo dpkg -i $WORKING_DIR/_tmp/google-chrome*.deb
