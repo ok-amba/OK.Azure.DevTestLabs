8645e8686bfd4c253eb0b55eeaeb77afdd2d1c0f
kaspertygesen <me@klabbe.net>
2020-03-10 12:29:14 +0100

    Create fix-mongo.sh

---
+++ linux-vm-docker-dotnetcore-nodejs/scripts/fix-mongo.sh
@@ -0,0 +1,3 @@
+echo "mongo-1-1 127.0.0.1" | sudo tee -a /etc/hosts > /dev/null
+echo "mongo-1-2 127.0.0.1" | sudo tee -a /etc/hosts > /dev/null
+echo "mongo-1-3 127.0.0.1" | sudo tee -a /etc/hosts > /dev/null
