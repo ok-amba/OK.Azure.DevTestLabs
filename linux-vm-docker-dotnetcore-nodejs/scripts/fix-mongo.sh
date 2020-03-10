3d43a502ed2f08e8fdfd84e050f133b984cd8c92
kaspertygesen <me@klabbe.net>
2020-03-10 12:57:53 +0100

    Update fix-mongo.sh

--- linux-vm-docker-dotnetcore-nodejs/scripts/fix-mongo.sh
+++ linux-vm-docker-dotnetcore-nodejs/scripts/fix-mongo.sh
@@ -1,3 +1,3 @@
-echo "mongo-1-1 127.0.0.1" | sudo tee -a /etc/hosts > /dev/null
-echo "mongo-1-2 127.0.0.1" | sudo tee -a /etc/hosts > /dev/null
-echo "mongo-1-3 127.0.0.1" | sudo tee -a /etc/hosts > /dev/null
+echo "127.0.0.1 mongo-1-1" | sudo tee -a /etc/hosts > /dev/null
+echo "127.0.0.1 mongo-1-2" | sudo tee -a /etc/hosts > /dev/null
+echo "127.0.0.1 mongo-1-3" | sudo tee -a /etc/hosts > /dev/null
