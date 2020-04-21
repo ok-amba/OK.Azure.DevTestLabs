240880c790b9ebfe6416bc3a8eb05a705a865a8a
kaspertygesen <me@klabbe.net>
2020-04-21 11:42:24 +0200

    Update install-nodejs.sh

--- linux-vm-docker-dotnetcore-nodejs/scripts/install-nodejs.sh
+++ linux-vm-docker-dotnetcore-nodejs/scripts/install-nodejs.sh
@@ -2,3 +2,6 @@
 #install npm & nodejs
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# dependency for puppeteer
+sudo apt-get install -y libgbm-dev
